b798a3667696f9f2c456d1e18d90a801de4d33e3
shishir1scp <shishir1scp@gmail.com>
2020-09-06 08:31:44 +0530

    creating multiple users from using map

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
-	state = {
-		id: "id",
-		login: "mojombo",
-		avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-		html_url: "https://github.com/mojombo",
-	};
-
 	render() {
-		const { login, avatar_url, html_url } = this.state;
+		const { login, avatar_url, html_url } = this.props.user;
 		//here we are destructuring
 		//here we are pulling stuffs(login,avatar_url,html_url) out from the object
 		return (
---
+++ src/components/users/Users.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import UserItem from "./UserItem";
+
+export class Users extends Component {
+	state = {
+		users: [
+			{
+				login: "mojombo",
+				id: 1,
+				avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+				html_url: "https://github.com/mojombo",
+			},
+			{
+				login: "defunkt",
+				id: 2,
+				avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
+				html_url: "https://github.com/defunkt",
+			},
+			{
+				login: "pjhyett",
+				id: 3,
+				avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
+				html_url: "https://github.com/pjhyett",
+			},
+		],
+	};
+	render() {
+		return (
+			<div>
+				{this.state.users.map((user) => (
+					<UserItem key={user.id} user={user} />
+					// here user is a prop
+				))}
+			</div>
+		);
+	}
+}
+
+export default Users;
